a60b63396f20f48959a7f588cb483a9382c5c892
MadhumithraA1426 <169076994+MadhumithraA1426@users.noreply.github.com>
2025-09-22 21:54:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,8 @@ def index():
         
     return render_template("index.html", caption=caption)
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
 
